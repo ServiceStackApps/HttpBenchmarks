c4851367269266909c2692cff3e08be70329f875
Demis Bellot <demis.bellot@gmail.com>
2014-04-21 01:24:33 -0400

    Add aws screenshots

--- build/ResultsView-deploy.bat
+++ build/ResultsView-deploy.bat
@@ -2,8 +2,7 @@
 SET MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 SET MSDEPLOY="C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe"
 SET AWSDEPLOY="C:\Program Files (x86)\AWS Tools\Deployment Tool\awsdeploy.exe"
-SET PKGPATH=C:\src\HttpBenchmarks\src\deploy
-SET DEPLOYPKG=%PKGPATH%\ResultsView-deploy.zip
+SET DEPLOYPKG=C:\src\HttpBenchmarks\src\deploy\ResultsView-deploy.zip
 
 %MSBUILD% ..\src\ResultsView\ResultsView.csproj /T:Package /property:PackageLocation=%DEPLOYPKG%
 
