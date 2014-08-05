529ce6b8946cb83e5c178054d3f924ba30ca3d79
Demis Bellot <demis.bellot@gmail.com>
2014-08-05 16:33:28 -0400

    Comment out Test Services

--- src/BenchmarksAnalyzer.ServiceInterface/TestServices.cs
+++ src/BenchmarksAnalyzer.ServiceInterface/TestServices.cs
@@ -2,25 +2,25 @@
 
 namespace BenchmarksAnalyzer.ServiceInterface
 {
-    [Route("/corstest")]
-    [Route("/corstest/{Value}")]
-    public class CorsTest
-    {
-        public string Value { get; set; }
-    }
+    //[Route("/corstest")]
+    //[Route("/corstest/{Value}")]
+    //public class CorsTest
+    //{
+    //    public string Value { get; set; }
+    //}
 
-    public class TestServices : Service
-    {
-        public object Any(CorsTest request)
-        {
-            string value = request.Value ?? "Bar";
-            return new HttpResult(request)
-            {
-                Headers = {
-                    { "X-Foo", value },
-                    { "Pragma", value },
-                }
-            };
-        }
-    }
+    //public class TestServices : Service
+    //{
+    //    public object Any(CorsTest request)
+    //    {
+    //        string value = request.Value ?? "Bar";
+    //        return new HttpResult(request)
+    //        {
+    //            Headers = {
+    //                { "X-Foo", value },
+    //                { "Pragma", value },
+    //            }
+    //        };
+    //    }
+    //}
 }
