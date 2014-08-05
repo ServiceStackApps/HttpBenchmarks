0f9f03d6429e404a3f1f8c67b3944a9868168872
Demis Bellot <demis.bellot@gmail.com>
2014-08-05 16:33:52 -0400

    Change UserSession to use the built-in session.GetProfileUrl()

--- src/BenchmarksAnalyzer.ServiceInterface/UserSession.cs
+++ src/BenchmarksAnalyzer.ServiceInterface/UserSession.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 using ServiceStack;
 using ServiceStack.Auth;
 
@@ -14,63 +15,12 @@ namespace BenchmarksAnalyzer.ServiceInterface
             base.OnRegistered(registerService);
         }
 
-        public override void OnAuthenticated(IServiceBase authService, IAuthSession session, IAuthTokens tokens, System.Collections.Generic.Dictionary<string, string> authInfo)
+        public override void OnAuthenticated(IServiceBase authService, IAuthSession session, 
+            IAuthTokens tokens, Dictionary<string, string> authInfo)
         {
             base.OnAuthenticated(authService, session, tokens, authInfo);
 
-            try
-            {
-                if (!this.Email.IsNullOrEmpty())
-                    this.ProfileUrl64 = this.Email.ToGravatarUrl(size: 64);
-
-                if (tokens.Provider == FacebookAuthProvider.Name)
-                {
-                    this.DisplayName = tokens.DisplayName ?? this.DisplayName;
-                    this.ProfileUrl64 = "http://avatars.io/facebook/{0}?size=medium".Fmt(this.UserName)
-                        .GetRedirectUrlIfAny() ?? this.ProfileUrl64;
-                }
-                else if (tokens.Provider == TwitterAuthProvider.Name)
-                {
-                    this.DisplayName = tokens.UserName ?? this.DisplayName;
-                    this.ProfileUrl64 = "http://avatars.io/twitter/{0}?size=medium".Fmt(this.UserName)
-                        .GetRedirectUrlIfAny() ?? this.ProfileUrl64;
-                }
-                else if (tokens.Provider == "GoogleOAuth")
-                {
-                    if (authInfo.ContainsKey("picture"))
-                        this.ProfileUrl64 = authInfo["picture"];
-                }
-                else if (tokens.Provider == "LinkedIn")
-                {
-                    //Ignore when deployed as cdn used doesn't support relative (ssl) schemes i.e. '//m.c.lnkd.licdn.com'
-                    if (HostContext.DebugMode)
-                    {
-                        if (authInfo.ContainsKey("picture"))
-                            this.ProfileUrl64 = authInfo["picture"];
-                    }
-                }
-            }
-            catch { }
-        }
-    }
-
-    public static class SessionExtensions
-    {
-        /// <summary>
-        /// Remove the userId from the url by using the anon CDN url
-        /// </summary>
-        public static string GetRedirectUrlIfAny(this string url)
-        {
-            var finalUrl = url;
-            try
-            {
-                var ignore = url.GetBytesFromUrl(
-                    requestFilter: req => req.AllowAutoRedirect = false,
-                    responseFilter: res => finalUrl = res.Headers[HttpHeaders.Location] ?? finalUrl);
-            }
-            catch { }
-
-            return finalUrl.EndsWith("avatar-64.png") ? null : finalUrl;
+            this.ProfileUrl64 = session.GetProfileUrl();
         }
     }
 
